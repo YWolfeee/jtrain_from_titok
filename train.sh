f226de8fe92bbc42400a5234fe2cbc958791774b
Qiyuan He <metahqy@163.com>
2024-12-22 16:21:22 +0800

    update exp settings

--- train.sh
+++ train.sh
@@ -1,8 +1,18 @@
+#PBS -N titok_matryoshka
+#PBS -S /bin/bash
+#PBS -l select=1:ncpus=52:mem=360gb:ngpus=4:host=cvml04
+
+nvidia-smi
+cd ~/jtrain_from_titok
+source ~/.bashrc
+eval "$(conda shell.bash hook)"
+conda activate titok
+
 export PYTHONPATH=$(pwd)
-WANDB_MODE=offline accelerate launch \
+accelerate launch \
     --num_machines=1 --num_processes=4 --machine_rank=0 \
     --main_process_ip=127.0.0.1 --main_process_port=9999 --same_network \
-    scripts/train_titok.py config=configs/training/stage1/titok_b64.yaml \
+    scripts/train_titok.py config=configs/training/stage1/titok_b64_matryoshka.yaml \
     experiment.project="titok_b64_stage1" \
     experiment.name="titok_b64_stage1_run1" \
     experiment.output_dir="titok_b64_stage1_run1" \
