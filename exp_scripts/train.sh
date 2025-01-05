7ecd9f11e933f3921aad39cc0eba4a20821779e1
Qiyuan He <metahqy@163.com>
2025-01-05 16:01:27 +0800

    add annealing

--- exp_scripts/train.sh
+++ exp_scripts/train.sh
@@ -1,6 +1,6 @@
 #PBS -N titok_matryoshka
 #PBS -S /bin/bash
-#PBS -l select=1:ncpus=52:mem=360gb:ngpus=4:host=cvml04
+#PBS -l select=1:ncpus=48:mem=360gb:ngpus=8:host=cvml11
 
 nvidia-smi
 cd ~/jtrain_from_titok
@@ -10,10 +10,10 @@ conda activate titok
 
 export PYTHONPATH=$(pwd)
 accelerate launch \
-    --num_machines=1 --num_processes=4 --machine_rank=0 \
+    --num_machines=1 --num_processes=8 --machine_rank=0 \
     --main_process_ip=127.0.0.1 --main_process_port=9999 --same_network \
-    scripts/train_titok.py config=configs/training/stage1/titok_b64_matryoshka.yaml \
-    experiment.project="titok_b64_stage1" \
-    experiment.name="titok_b64_stage1_run1" \
-    experiment.output_dir="titok_b64_stage1_run1" \
+    scripts/train_titok.py config=configs/training/stage1/titok_s128_matryoshka_annealing.yaml \
+    experiment.project="titok_s128_matryoshka_annealing_stage1" \
+    experiment.name="titok_s128_matryoshka_annealing_stage1_run1" \
+    experiment.output_dir="titok_s128_matryoshka_annealing_stage1_run1" \
     training.per_gpu_batch_size=32
--- utils/train_utils.py
+++ utils/train_utils.py
@@ -362,6 +362,11 @@ def train_one_epoch(config, logger, accelerator,
         else:
             proxy_codes = None
 
+        # QY: Update max mask rate based on annealing schedule if configured
+        if config.model.reconstruction_regularization.get("annealing"):
+            max_mask_rate = get_titok_max_mask_rate(config, global_step)
+            accelerator.unwrap_model(model).set_max_mask_rate(max_mask_rate)
+
         with accelerator.accumulate([model, loss_module]):
             reconstructed_images, extra_results_dict = model(images)
             if proxy_codes is None:
@@ -576,6 +581,17 @@ def train_one_epoch(config, logger, accelerator,
 
     return global_step
 
+def get_titok_max_mask_rate(config, global_step):
+    """QY: Get the max mask rate for TiTok model."""
+    annealing = config.model.reconstruction_regularization.annealing
+    time_start = annealing.time_start * config.training.max_train_steps
+    time_end = annealing.time_end * config.training.max_train_steps
+    if global_step < time_start:
+        return 0.0
+    elif global_step > time_end:
+        return config.model.reconstruction_regularization.max_mask_rate
+    else:
+        return (global_step - time_start) / (time_end - time_start) * config.model.reconstruction_regularization.max_mask_rate
 
 def get_rar_random_ratio(config, cur_step):
     randomness_anneal_start = config.model.generator.randomness_anneal_start
