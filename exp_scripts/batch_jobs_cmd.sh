9c078ab6855bdd1cc7e672197fcbab57b5134055
Haotian Ye <YWolfeee@outlook.com>
2025-02-05 03:37:53 -0800

    modify slurm job arguments to support large scale running for distortion-rate tradeoff loss. Submitted 16 jobs. Unsure whether it will succeed or not. Unsure why right now rate loss is almost the same across different data point

--- exp_scripts/batch_jobs_cmd.sh
+++ exp_scripts/batch_jobs_cmd.sh
@@ -10,19 +10,20 @@ config_files=(
 )
 
 # Parameters to iterate over for each YAML file
-settings=(
+# settings=(
     # "use_reconstruction_regularization=False use_annealing=False use_self_distilliation=False"
-    "use_reconstruction_regularization=True use_annealing=False use_self_distilliation=False"
+    # "use_reconstruction_regularization=True use_annealing=False use_self_distilliation=False"
     # "use_reconstruction_regularization=True use_annealing=False use_self_distilliation=True"
     # "use_reconstruction_regularization=True use_annealing=True use_self_distilliation=False"
     # "use_reconstruction_regularization=True use_annealing=True use_self_distilliation=True"
-)
+# )
 
 # Define other parameters
+use_reconstruction_regularization=True
 per_gpu_batch_size=64
 learning_rate=4e-4
-is_increasing=True
-output_root="results_try_hierarchical_loss"
+is_increasing=False
+output_root="results_try_distortion_rate_tradeoff_loss"
 
 # Loop through each YAML file
 for config_file in "${config_files[@]}"; do
@@ -30,19 +31,25 @@ for config_file in "${config_files[@]}"; do
     config_name=$(basename "$config_file" .yaml)
 
     # Loop through each setting combination
-    for setting in "${settings[@]}"; do
+    # for setting in "${settings[@]}"; do
         # Evaluate the settings to create variables dynamically
-        eval $setting
+        # eval $setting
+    for use_policy_annealing in "True" "False"; do
+        for rate_weight in 0.01 0.05 0.1 0.5; do
+            for policy_network in "mlp" "transformer"; do
 
 
-        # Dynamically create the job name
-        jobname="${config_name}+continue_train+lr=4e-4+use_ours=${use_reconstruction_regularization}+use_annealing=${use_annealing}+is_increasing=${is_increasing}+use_self_distilliation=${use_self_distilliation}"
+                # Dynamically create the job name
+                jobname="${config_name}+lr=4e-4+use_ours=${use_reconstruction_regularization}+use_policy_annealing=${use_policy_annealing}+rate_weight=${rate_weight}+policy_network=${policy_network}"
 
-        echo "jobname = $jobname"
-        # Submit the job
-        command="sbatch --job-name=$jobname --output='$output_root/$jobname/logs/slurm_%j.out' \
-            exp_scripts/long_slurm.sh $config_name $per_gpu_batch_size $learning_rate $use_reconstruction_regularization $use_annealing $is_increasing $use_self_distilliation $output_root"
-        echo "$command"
-        eval "$command"
+                echo "jobname = $jobname"
+                # Submit the job
+                command="sbatch --job-name=$jobname --output='$output_root/$jobname/logs/slurm_%j.out' \
+                    exp_scripts/long_slurm.sh $config_name $per_gpu_batch_size $learning_rate $use_reconstruction_regularization $use_policy_annealing $rate_weight $policy_network $output_root"
+                echo "$command"
+                eval "$command"
+                # exit
+            done
+        done
     done
 done
