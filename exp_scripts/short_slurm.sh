924724831c8fc3e053df60b3b746c295047f43c7
Haotian Ye <YWolfeee@outlook.com>
2025-02-16 22:08:34 -0800

    debug

--- exp_scripts/short_slurm.sh
+++ exp_scripts/short_slurm.sh
@@ -14,14 +14,14 @@ source ~/.bashrc
 
 config_name="titok_b256_4096_12"
 model_type="transformer"
-tag="debug_new_design"
+tag="debug_new_design_no_policy_encoder_condition"
 ngpus=8
 export PYTHONPATH=$(pwd)
 
-# accelerate launch \
-#     --num_machines=1 --num_processes=${ngpus} --machine_rank=0 \
-#     --main_process_ip=127.0.0.1 --main_process_port=9999 --same_network \
-python -m debugpy --listen 0.0.0.0:5678 --wait-for-client \
+# python -m debugpy --listen 0.0.0.0:5678 --wait-for-client \
+accelerate launch \
+    --num_machines=1 --num_processes=${ngpus} --machine_rank=0 \
+    --main_process_ip=127.0.0.1 --main_process_port=9999 --same_network \
     scripts/train_titok.py config=configs/training/stage1/${config_name}.yaml \
     experiment.project="TEMP_QY" \
     experiment.name="${config_name}_${tag}" \
@@ -36,7 +36,7 @@ python -m debugpy --listen 0.0.0.0:5678 --wait-for-client \
     model.reconstruction_regularization.annealing.time_end=0.1 \
     model.reconstruction_regularization.annealing.is_increasing=False \
     \
-    model.reconstruction_regularization.use_policy=True \
+    model.reconstruction_regularization.use_policy=False \
     model.reconstruction_regularization.policy.model_type=${model_type} \
     model.reconstruction_regularization.policy.num_heads=4 \
     model.reconstruction_regularization.policy.hidden_size=128 \
