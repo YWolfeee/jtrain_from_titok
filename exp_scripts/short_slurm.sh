141e301e871e9dac922fbbfc5ff80f27f751b1ad
Haotian Ye <YWolfeee@outlook.com>
2025-02-18 20:36:35 -0800

    debug

--- exp_scripts/short_slurm.sh
+++ exp_scripts/short_slurm.sh
@@ -14,18 +14,18 @@ source ~/.bashrc
 
 config_name="titok_b256_4096_12"
 model_type="transformer"
-tag="debug_new_design_cat256"
+tag="debug_gaussian_nan"
 ngpus=8
 export PYTHONPATH=$(pwd)
 
-# python -m debugpy --listen 0.0.0.0:5678 --wait-for-client \
-accelerate launch \
-    --num_machines=1 --num_processes=${ngpus} --machine_rank=0 \
-    --main_process_ip=127.0.0.1 --main_process_port=9999 --same_network \
+# accelerate launch \
+#     --num_machines=1 --num_processes=${ngpus} --machine_rank=0 \
+#     --main_process_ip=127.0.0.1 --main_process_port=9999 --same_network \
+python -m debugpy --listen 0.0.0.0:5678 --wait-for-client \
     scripts/train_titok.py config=configs/training/stage1/${config_name}.yaml \
     experiment.project="TEMP_QY" \
     experiment.name="${config_name}_${tag}" \
-    experiment.output_dir="temp/${config_name}_${tag}" \
+    experiment.output_dir="temp/${tag}" \
     model.use_reconstruction_regularization=True \
     model.reconstruction_regularization.name='matryoshka' \
     model.reconstruction_regularization.mask_ratio_method='hierarchical' \
@@ -48,11 +48,11 @@ accelerate launch \
     model.reconstruction_regularization.gumbel_softmax.hard=True \
     model.reconstruction_regularization.gumbel_softmax.fix_tau=False \
     \
-    model.reconstruction_regularization.policy.annealing.use_annealing=True \
+    model.reconstruction_regularization.policy.annealing.use_annealing=False \
     model.reconstruction_regularization.policy.annealing.alpha_start=0.02 \
     model.reconstruction_regularization.policy.annealing.alpha_end=1.0 \
     model.reconstruction_regularization.policy.feature_extractor_name="facebook/dinov2-base" \
-    model.reconstruction_regularization.policy.logit_head_type="categorical_8" \
+    model.reconstruction_regularization.policy.logit_head_type="gaussian_1" \
     \
     model.reconstruction_regularization.policy.temperature.use_T=False \
     model.reconstruction_regularization.policy.temperature.T0=10000 \
@@ -66,9 +66,8 @@ accelerate launch \
     training.max_train_steps=250_000 \
     dataset.params.train_shards_path_or_url='datasets/imagenet-train-{000000..000252}.tar' \
     dataset.params.eval_shards_path_or_url='datasets/imagenet-val-{000000..000009}.tar' \
-
-
-    # experiment.init_weight="results_try_tradeoff_with_activation/titok+config=titok_b${shape}_4096_12/checkpoint-250000/unwrapped_model/pytorch_model.bin"
+    
+    # experiment.init_weight="results_try_new_design/gaussian+rate_weight=1+policy_network=transformer+anneal_policy+alpha_start=2/checkpoint-30000/unwrapped_model/pytorch_model.bin"
 
     # \
     # dataset.params.train_shards_path_or_url='small_datasets/imagenet-train-000000.tar' \
