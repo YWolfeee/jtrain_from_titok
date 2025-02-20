a9ee7489e11aa9e004412ab2725efef110a8b240
Qiyuan He <metahqy@163.com>
2025-02-20 17:13:04 +0800

    add vae_results train/eval json and data usage

--- exp_scripts/preget_nll.sh
+++ exp_scripts/preget_nll.sh
@@ -1,9 +1,11 @@
 #PBS -N preget_nll
 #PBS -S /bin/bash
-#PBS -l select=1:ncpus=24:mem=180gb:ngpus=4:host=cvml10
+#PBS -l select=1:ncpus=12:mem=90gb:ngpus=1:host=cvml10
 
 config_name='titok_b128_4096_12'
-tag="preget_nll_trial" # tag="preget_nll_trial_distributed_dataset_test"
+start_batch=0
+dataset_split="train"
+tag="preget_nll_trial_after${start_batch}_${dataset_split}_cvml10"
 
 nvidia-smi
 cd ~/jtrain_from_titok
@@ -15,7 +17,7 @@ export PYTHONPATH=$(pwd)
 export WANDB_INIT_TIMEOUT=300
 
 WANDB_MODE=offline accelerate launch \
-    --num_machines=1 --num_processes=4 --machine_rank=0 \
+    --num_machines=1 --num_processes=1 --machine_rank=0 \
     --main_process_ip=127.0.0.1 --main_process_port=9999 --same_network \
     scripts/preget_nll.py config=configs/training/stage1/${config_name}.yaml \
     experiment.project="TEMP_QY" \
@@ -23,4 +25,6 @@ WANDB_MODE=offline accelerate launch \
     experiment.output_dir="temp/${tag}" \
     dataset.params.train_shards_path_or_url="/mnt/rdata8/imagenet_wds/imagenet-train-{000000..000252}.tar" \
     dataset.params.eval_shards_path_or_url="/mnt/rdata8/imagenet_wds/imagenet-val-{000000..000009}.tar" \
-    training.per_gpu_batch_size=32
+    training.per_gpu_batch_size=32 \
+    start_batch=${start_batch} \
+    dataset_split=${dataset_split}
